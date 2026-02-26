8836fc5b5736bfe54b1caa4472603a49f82f730b
m-chup <firemac908@gmail.com>
2026-02-26 15:41:17 -0800

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main() {
 
-   /* Type your code here. */
+   int integer1, integer2;
+   cout << "Enter two integers." << endl;
+   cin >> integer1;
+   cin >> integer2;
+
+   if (integer2 < integer1)
+   {
+    cout << "Second integer can't be less than the first." << endl;
+    cout << "Please run the program again." << endl;
+   }
+   else
+   {
+    for (; integer1 <= integer2; integer1 += 5)
+    {
+        cout << integer1 << " ";
+    }
+   }
+   cout << endl;
 
    return 0;
 }
